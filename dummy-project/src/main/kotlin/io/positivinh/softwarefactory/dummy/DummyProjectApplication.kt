3e199f5e141de7a2d46726efdd42359f3699df26
vinh <positivinh@gmail.com>
2024-12-24 22:19:13 +0100

    feat: software factory BOM and parent POM

---
+++ dummy-project/src/main/kotlin/io/positivinh/softwarefactory/dummy/DummyProjectApplication.kt
@@ -0,0 +1,12 @@
+package io.positivinh.softwarefactory.dummy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DummyProjectApplication
+
+fun main(args: Array<String>) {
+
+    runApplication<DummyProjectApplication>(*args)
+}
